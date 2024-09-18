ccba936881006c479967117668ac2594ebc0b886
OussamaDaanouni <dondaanouni@gmail.com>
2024-09-18 14:51:10 +0000

    Adding all variables to chart

--- User_Interface/streamlit_ui/profiling.py
+++ User_Interface/streamlit_ui/profiling.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import plotly.graph_objects as go
-import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 import plotly.express as px
 
@@ -21,9 +20,12 @@ def profiling(df_merged, option_site, option_transaction):
         else:
             df_selected_city = df_merged[(df_merged['ville'] == selected_city) & (df_merged['real_estate_type'] == selected_real_estate)]
 
+
+    st.write(df_selected_city)
     # Selecting extras features for each DB
     if (option_site == 'avito') and (option_transaction == 'sell'):
         boolean_features = df_merged.columns[14:28]
+
     else:
         boolean_features = df_merged.columns[13:27]
 
@@ -33,26 +35,98 @@ def profiling(df_merged, option_site, option_transaction):
     # Prepare the aggregation logic for boolean features
     boolean_agg = {feature: (feature, lambda x: (x == 'Yes').mean()) for feature in boolean_features}
 
-    # Aggregate key metrics by neighborhood (Secteur)
-    df_profile = df_selected_city.groupby('secteur').agg(
-        mean_price=('price', 'mean'),
-        avg_surface_totale=('surface_totale', 'mean'),
-        avg_chambres=('chambres', 'mean'),
-        **boolean_agg  # Add the dynamically created boolean feature aggregations
-    ).reset_index()
+    if (option_site == 'avito') and (option_transaction == 'rent'):
+        # Aggregate key metrics by neighborhood (Secteur)
+        df_profile = df_selected_city.groupby('secteur').agg(
+            mean_price=('price', 'mean'),
+            avg_surface_totale=('surface_totale', 'mean'),
+            avg_surface_habitable=('surface_habitable', 'mean'),
+            avg_chambres=('chambres', 'mean'),
+            avg_salons=('salons', 'mean'),
+            avg_pieces=('pieces', 'mean'),
+            avg_salle_bains=('salle_bains', 'mean'),
+            avg_etage=('etage', 'mean'),
+            **boolean_agg  # Add the dynamically created boolean feature aggregations
+        ).reset_index()
 
-    # Normalize continuous features (price, surface, rooms)
-    scaler = MinMaxScaler()
-    df_profile[['mean_price', 'avg_surface_totale', 'avg_chambres']] = scaler.fit_transform(
-        df_profile[['mean_price', 'avg_surface_totale', 'avg_chambres']]
-    )
+        # Normalize continuous features (price, surface, rooms)
+        scaler = MinMaxScaler()
+        df_profile[['mean_price', 'avg_surface_totale', 'avg_surface_habitable', 'avg_chambres', 'avg_salons', 'avg_pieces','avg_salle_bains','avg_etage']] = scaler.fit_transform(
+            df_profile[['mean_price', 'avg_surface_totale' , 'avg_surface_habitable', 'avg_chambres', 'avg_salons', 'avg_pieces','avg_salle_bains','avg_etage']]
+        )
 
-    # Select multiple neighborhoods (Secteurs) to profile
-    selected_neighborhoods = st.multiselect("Select neighborhoods to compare", df_profile['secteur'].unique())
+        # Select multiple neighborhoods (Secteurs) to profile
+        selected_neighborhoods = st.multiselect("Select neighborhoods to compare", df_profile['secteur'].unique())
 
-    # Categories to include in the spider chart (continuous + dynamic boolean features)
-    categories = ['mean_price', 'avg_surface_totale', 'avg_chambres'] + boolean_features
-    categories.append(categories[0])  # Append the first category to close the loop
+        # Categories to include in the spider chart (continuous + dynamic boolean features)
+        categories = ['mean_price', 'avg_surface_totale', 'avg_surface_habitable', 'avg_chambres', 'avg_salons', 'avg_pieces','avg_salle_bains','avg_etage'] + boolean_features
+        categories.append(categories[0])  # Append the first category to close the loop
+    
+    elif (option_site == 'avito') and (option_transaction == 'sell'):
+        # Calculate percentage of each category in the qualitative variable 'age_category'
+        age_categories = ['Neuf', '1-5 ans', '6-10 ans', '11-20 ans', '21+ ans']
+        age_agg = {f'perc_{category}': ('age_bien', lambda x, category=category: (x == category).mean()) for category in age_categories}
+
+        agg_dict = {**boolean_agg, **age_agg}
+
+        # Aggregate key metrics by neighborhood (Secteur)
+        df_profile = df_selected_city.groupby('secteur').agg(
+            mean_price=('price', 'mean'),
+            avg_surface_totale=('surface_totale', 'mean'),
+            avg_surface_habitable=('surface_habitable', 'mean'),
+            avg_chambres=('chambres', 'mean'),
+            avg_salons=('salons', 'mean'),
+            avg_pieces=('pieces', 'mean'),
+            avg_salle_bains=('salle_bains', 'mean'),
+            avg_etage=('etage', 'mean'),
+            **agg_dict  # Add the merged boolean and age category aggregations
+        ).reset_index()
+
+        # Normalize continuous features (price, surface, rooms)
+        scaler = MinMaxScaler()
+        df_profile[['mean_price', 'avg_surface_totale', 'avg_surface_habitable', 'avg_chambres', 'avg_salons', 'avg_pieces','avg_salle_bains','avg_etage']] = scaler.fit_transform(
+            df_profile[['mean_price', 'avg_surface_totale' , 'avg_surface_habitable', 'avg_chambres', 'avg_salons', 'avg_pieces','avg_salle_bains','avg_etage']]
+        )
+
+        # Select multiple neighborhoods (Secteurs) to profile
+        selected_neighborhoods = st.multiselect("Select neighborhoods to compare", df_profile['secteur'].unique())
+
+        # Categories to include in the spider chart (continuous + dynamic boolean features)
+        categories = ['mean_price', 'avg_surface_totale', 'avg_surface_habitable', 'avg_chambres', 'avg_salons', 'avg_pieces','avg_salle_bains','avg_etage'] + boolean_features + [f'perc_{category}' for category in age_categories]
+        categories.append(categories[0])  # Append the first category to close the loop
+
+    else:
+        # Calculate percentage of each category in the qualitative variable 'age_category'
+        age_categories = ["Moins d'un an", '1-5 ans', '5-10 ans', '10-20 ans', '20-30 ans']
+        age_agg = {f'perc_{category}': ('age_bien', lambda x, category=category: (x == category).mean()) for category in age_categories}
+
+        etat_categories = ["Nouveau", "Bon état", "À rénover"]
+        etat_agg = {f'perc_{category}': ('etat', lambda x, category=category: (x == category).mean()) for category in etat_categories}
+        agg_dict = {**boolean_agg, **age_agg , **etat_agg}
+
+        # Aggregate key metrics by neighborhood (Secteur)
+        df_profile = df_selected_city.groupby('secteur').agg(
+            mean_price=('price', 'mean'),
+            avg_surface_totale=('surface_totale', 'mean'),
+            avg_chambres=('chambres', 'mean'),
+            avg_pieces=('pieces', 'mean'),
+            avg_salle_bains=('salle_bains', 'mean'),
+            avg_etage=('etage', lambda x: x[x != 'Not_defined'].astype(float).mean()),  # Ignore 'Not_defined'
+            **agg_dict  # Add the merged boolean and age category aggregations
+        ).reset_index()
+
+        # Normalize continuous features (price, surface, rooms)
+        scaler = MinMaxScaler()
+        df_profile[['mean_price', 'avg_surface_totale', 'avg_chambres', 'avg_pieces','avg_salle_bains','avg_etage']] = scaler.fit_transform(
+            df_profile[['mean_price', 'avg_surface_totale' , 'avg_chambres', 'avg_pieces','avg_salle_bains','avg_etage']]
+        )
+
+        # Select multiple neighborhoods (Secteurs) to profile
+        selected_neighborhoods = st.multiselect("Select neighborhoods to compare", df_profile['secteur'].unique())
+
+        # Categories to include in the spider chart (continuous + dynamic boolean features)
+        categories = ['mean_price', 'avg_surface_totale', 'avg_chambres', 'avg_pieces','avg_salle_bains','avg_etage'] + boolean_features + [f'perc_{category}' for category in age_categories] + [f'perc_{category}' for category in etat_categories]
+        categories.append(categories[0])  # Append the first category to close the loop
 
     # Get a color palette for the number of selected neighborhoods
     color_palette = px.colors.qualitative.Plotly  # You can use other palettes like 'D3', 'Set1', etc.
